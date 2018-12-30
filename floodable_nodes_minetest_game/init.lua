9982d68012888e924f81a741bfed6d34dd0c04d1
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-12-30 17:44:25 +0000

    floodable_nodes_minetest_game: [-] add a more robust method for dropping papyrus

--- floodable_nodes_minetest_game/init.lua
+++ floodable_nodes_minetest_game/init.lua
@@ -38,14 +38,41 @@ end
 
 
 
+
+
 -- papyrus is a little bit more complex due to it's cascading behaviour;
 -- to call it's after_dig_node function we'd normally have to pass some data in,
 -- notably a digger object that quacks enough like a player to not crash.
--- fortunately minetest.dig_node takes care of that for us here.
-local ghost_dig = minetest.on_flood_dig_node
+-- minetest.dig_node does not appear to work in multiplayer for some weird reason;
+-- it will seemingly mysteriously fail even in the absence of protection mods,
+-- so we can't really use it.
+-- instead we emulate the loop of digging up, sans requiring a digger object.
+local gn = minetest.get_node
+local drop = minetest.on_flood_drop_self
+local remove = minetest.remove_node
+local on_flood_dig_up_ = function(nodename)
+	local function dig_recursive(pos, oldnode, newnode)
+		-- we already know we are a valid node here;
+		-- we perform the check for the next one.
+		drop(pos, oldnode, newnode)
+
+		pos.y = pos.y + 1
+		local nextnode = gn(pos)
+		-- return false to work as an on_flood callback and break recursion
+		if nextnode.name ~= nodename then return false end
+		-- clear out that node as we haven't already done that.
+		-- the base node will be replaced by the flooding liquid.
+		remove(pos)
+
+		return dig_recursive(pos, nextnode, newnode)
+	end
+
+	return dig_recursive
+end
+
 local custom_patches = {
 	["default:papyrus"] = {
-		on_flood = ghost_dig,
+		on_flood = on_flood_dig_up_("default:papyrus"),
 	}
 }
 for node, patch in pairs(custom_patches) do
